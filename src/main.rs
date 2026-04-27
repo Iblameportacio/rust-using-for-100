3ff8deabb2ebdded96b933ccf84d62be77f1511e
Iblameportacio <gm.portacio@gmail.com>
2026-04-27 00:36:00 -0500

    multiplos de 5 de mas a menos

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-//Imprime los múltiplos de 3 entre 1 y 90.
+//Imprime los múltiplos de 5 entre 1 y 100 en orden descendente.
 fn main() {
-    for i in 1..=90 {
-        if i % 3 == 0 {
+    for i in (1..=100).rev() {
+        if i % 5 == 0 {
             println!("{}", i)
         }
     }

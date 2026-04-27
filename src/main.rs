2e27ca9f6f8ac6dffa1d5b836009941cc20e5267
Iblameportacio <gm.portacio@gmail.com>
2026-04-27 11:42:40 -0500

    cubos del 1 al 15

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-//Imprime el cuadrado ($n^2$) de los números del 1 al 20.
+//Imprime el cubo ($n^3$) de los números del 1 al 15.
 fn main() {
-    for i in 1..=20 {
+    for i in 1..=15 {
         let inicio: i32 = i;
-        let cuadrado = inicio.pow(2);
+        let cuadrado = inicio.pow(3);
         println!("{}", cuadrado)
     }
 }

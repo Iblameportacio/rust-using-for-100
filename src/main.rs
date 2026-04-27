38c405c3b16630055c7b140c97a0df1bff59a52f
Iblameportacio <gm.portacio@gmail.com>
2026-04-26 22:50:54 -0500

    numeros impares del 1 al 200

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-//Imprime solo los números pares del 1 al 200.
+//IImprime solo los números impares del 1 al 200.
 fn main(){
     for i in 1..=200 {
-        if i % 2 ==0{println!("{}", i)}
+        if i % 3 ==0{println!("{}", i)}
     }
 }

0cffca1cb9724b8b81b58dcec09eca7632e79680
Iblameportacio <gm.portacio@gmail.com>
2026-04-26 22:49:05 -0500

    numeros pares del 1 al 200

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-//Imprime números del 100 al 1.
+//Imprime solo los números pares del 1 al 200.
 fn main(){
-    for i in (1..=100).rev() {
-        println!("{}", i);
+    for i in 1..=200 {
+        if i % 2 ==0{println!("{}", i)}
     }
 }

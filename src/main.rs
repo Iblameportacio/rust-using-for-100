5cd0b4457183bbb10062f28d21006af8a3e6553f
Iblameportacio <gm.portacio@gmail.com>
2026-04-27 00:31:44 -0500

    suma pares del 1 al 100

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-//Suma de los primeros 50 números naturales.
+//Suma de todos los números pares entre 1 y 100.
 fn main() {
     let mut inicio = 0;
-    for i in 1..=50 {
-        inicio = inicio + i;
+    for i in 1..=100 {
+        if i % 2 == 0 {
+            inicio = inicio + i;
+        }
     }
     println!("{}", inicio)
 }

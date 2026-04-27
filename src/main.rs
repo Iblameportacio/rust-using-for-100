1c2457c723ce3472a01ba0639c8e6e7dcf7c7f0e
Iblameportacio <gm.portacio@gmail.com>
2026-04-27 00:14:48 -0500

    tabla de un numero ingresado por el user

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-//Tabla de multiplicar del 5.
-fn main(){
-    let num: i32 = 5;
-    for i in 0..=12{
-        let solucion = num * i;
-        println!("{:?} * {:?} = {:?}", num, i, solucion);
-    }
+//Tabla de multiplicar de un número ingresado por el usuario.
+use std::io;
+fn main() {
+    println!("Por favor, introduce un numero:");
+    let mut input = String::new();
 
+    io::stdin().read_line(&mut input).expect("error");
+
+    let num: i32 = input.trim().parse().expect("no es número");
+    for i in 0..=12 {
+        let solucion = num * i;
+        println!("{} * {} = {}", num, i, solucion);
+    }
 }

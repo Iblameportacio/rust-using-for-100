ef199843d2302647575a01fa0631856893ece97d
Iblameportacio <gm.portacio@gmail.com>
2026-04-28 00:31:27 -0500

    hola rust 50 veces

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-//Imprime el cubo ($n^3$) de los números del 1 al 15.
+//Imprime "Hola Rust" 50 veces, numerando cada línea.
 fn main() {
-    for i in 1..=15 {
-        let inicio: i32 = i;
-        let cuadrado = inicio.pow(3);
-        println!("{}", cuadrado)
+    let hola = "hola rust";
+    for i in 1..=50 {
+        println!("{:?} {:?}", i, hola)
     }
 }

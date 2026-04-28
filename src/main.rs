95ac9d49372324c8f67dc454be74acb0a39f8e8c
Iblameportacio <gm.portacio@gmail.com>
2026-04-28 01:36:46 -0500

    nivel 1 completado ^^|||

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 //Imprime los números del 1 al 100, pero sáltate el 13 y el 66.
 fn main() {
-    for i in 1..=100 {
-        if i == 13 || i == 66 {
-            continue;
-        }
-        println!("{}", i)
-    }
+    println!("nivel 1 completado");
 }

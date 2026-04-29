7c13f30a37355d3f9cb2837cf0f9782f36b0a7c6
Iblameportacio <gm.portacio@gmail.com>
2026-04-28 22:37:08 -0500

    factorial de un n dado

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-//Imprime el factorial de 7
+//Calcula el factorial de un número n dado.
+use std::io;
 fn main() {
-    let mut inicio = 1;
-    for i in 1..=7 {
-        inicio *= i;
+    let mut resultado = 1;
+    println!("escriba un numero");
+    let mut n = String::new();
+    io::stdin()
+        .read_line(&mut n)
+        .expect("fallo, escribe un numero.");
+    let n: i32 = n.trim().parse().expect("no es número");
+    for i in 1..=n {
+        resultado *= i;
     }
-    println!("el factorial de 7 es: {}", inicio)
+    println!("el factorial de {:?} es: {:?}", n, resultado)
 }

30a2578b1ea08f513f48004c70516178eeaee316
Iblameportacio <gm.portacio@gmail.com>
2026-04-30 05:58:17 -0500

    los primeros 4 numeros perfectos

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-//Calcula la suma de una progresión aritmética ($a, a+d, a+2d...$) de 10 términos.
+//Imprime los primeros 4 números perfectos.
 fn main() {
-    let a = 2;
-    let d = 3;
-    let mut contador = 0;
-    for i in 0..10 {
-        let cumulus = a + i * d;
-        contador += cumulus;
+    println!(
+        "Un número perfecto es aquel que es igual a la
+        suma de sus divisores propios (excluyéndose a sí mismo)"
+    );
+    for z in 1..=10000 {
+        let mut suma = 0;
+        for n in 1..z {
+            if z % n == 0 {
+                suma += n;
+            }
+        }
+        if suma == z {
+            println!("{}", z);
+        }
     }
-    println!("{}", contador)
 }

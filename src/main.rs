4ca7694397f8c2b150a26b39d75fb35eb7c105f3
Iblameportacio <gm.portacio@gmail.com>
2026-04-30 21:16:19 -0500

    imprima una linea de 20 *

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-//Calcula la media aritmética de los números del 1 al n
+//Imprime una línea de 20 asteriscos *
 fn main() {
-    let n = 10;
-    let mut suma = 0;
-    for i in 1..=n {
-        suma += i;
+    for _i in 1..=20 {
+        print!("*");
     }
-    println!("la suma es: {}", suma);
-    let media_aritmetica: f32 = suma as f32 / n as f32;
-    println!("la media aritmetica es: {}", media_aritmetica);
 }

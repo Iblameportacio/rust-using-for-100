96bef143fd83b50f4a0ee626efd73e04eafa9fb1
Iblameportacio <gm.portacio@gmail.com>
2026-05-02 06:31:15 -0500

    level 2 completed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-// Imprime los números del 1 al 100,
-//  pero sustituye los múltiplos de 4 por la palabra "Cuatro".
 fn main() {
-    for i in 1..=100 {
-        if i % 4 == 0 {
-            println!("cuatro");
-        } else {
-            println!("{}", i);
-        }
-    }
+    println!("nivel 2 completado ^|")
 }

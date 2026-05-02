13b82ea49e940a899afaa8eaa8cb4cf34866b80f
Iblameportacio <gm.portacio@gmail.com>
2026-05-01 20:09:04 -0500

    triangulo rectangulo de asteriscos

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+//Imprime un triángulo rectángulo de asteriscos de altura 5.
 fn main() {
     for _fila in 1..=5 {
-        for _5 in 1..=5 {
+        for _5 in 1..=_fila {
             print!("*");
         }
-        println!(); //← salto de línea
+        println!("");
     }
 }

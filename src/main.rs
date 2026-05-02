42a324852cf92b7c509900eb096971b810de9297
Iblameportacio <gm.portacio@gmail.com>
2026-05-02 16:59:36 -0500

    suma de numeros dentro de un vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+//Crea un vector del 1 al 10 y súmalo.
 fn main() {
-    println!("nivel 3")
+    let mut inicio = 0;
+    let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for i in v {
+        inicio += i;
+    }
+    println!("la suma total del vector es: {}", inicio);
 }

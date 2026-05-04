ca3b12548a4cd940241bf97e91007b441e9409d8
Iblameportacio <gm.portacio@gmail.com>
2026-05-03 21:11:25 -0500

    invertir un vector con un bucle for

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-//Cuenta cuántas veces aparece un número específico en un vector.
+//Invierte un vector manualmente usando un bucle.
 fn main() {
     let v = vec![
         1, 2, 3, 4, 6, 32, 4, 667, 9, 8, 8236, 3132, 5, 451, 654, 613341, 1, 1, 1, 1, 1,
     ];
-    let mut contador = 0;
-    let numero = 1;
-    for i in v {
-        //im not gonna use &v because im not going use the vector again in this problem
-        if numero == i {
-            contador += 1;
-        }
+    let vec_rev_iter = v.iter().rev();
+    for i in vec_rev_iter {
+        println!("{}", i);
     }
-    println!("hay {} unos en el vector ", contador);
 }

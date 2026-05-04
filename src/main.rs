d0d4d1fd4c1d3d32fa3925c1aded4e88824a9c6c
Iblameportacio <gm.portacio@gmail.com>
2026-05-03 19:51:23 -0500

    el numero menor en un vector

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-//Encuentra el número mayor en un vector de 20 números aleatorios.
+//Encuentra el número menor en un vector.
 fn main() {
     let v = vec![
         1, 10, 23, 14, 4, 2, 3, 6, 57, 35, 2, 56, 9, 8279, 98, 670, 12, 21, 34, 69,
     ];
-    let mut mayor = v[0];
+    let mut menor = v[0];
     for i in v {
-        if mayor < i {
-            mayor = i;
+        if menor > i {
+            menor = i;
         }
     }
-    println!("{}", mayor)
+    println!("{}", menor);
 }

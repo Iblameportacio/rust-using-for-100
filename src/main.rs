160440a246d32f9519bd6ad9ce37d7f0b0a6e0d1
Iblameportacio <gm.portacio@gmail.com>
2026-05-03 22:37:02 -0500

    invertir un vector

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let v = vec![
         1, 2, 3, 4, 6, 32, 4, 667, 9, 8, 8236, 3132, 5, 451, 654, 613341, 1, 1, 1, 1, 1,
     ];
-    let vec_rev_iter = v.iter().rev();
+    let vec_rev_iter: Vec<_> = v.iter().rev().collect();
     for i in vec_rev_iter {
         println!("{}", i);
     }

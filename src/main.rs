586d9244b47b303d8c190554d08f7beef7da64b5
Iblameportacio <gm.portacio@gmail.com>
2026-05-08 23:50:03 -0500

    no c

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-//Encuentra la posición (índice) de un elemento en un vector.
 fn main() {
-    let v = vec![1, 2, 3, 4, 5, 6];
-    let buscar_este_numero = 6;
-    for i in 0..v.len() {
-        if v[i] == buscar_este_numero {
-            println!(
-                "el numero {} esta en la posicion v[{}]",
-                buscar_este_numero, i
-            )
-        }
-    }
+    println!("para no perder la racha de commits lmao")
 }

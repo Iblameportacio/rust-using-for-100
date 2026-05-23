abc4d064aea5592c5074441d941b76ddde787a40
Iblameportacio <gm.portacio@gmail.com>
2026-05-23 15:37:50 -0500

    func q rcbe n eleva a 3 y multplica solo los pares

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 //Crea una función que reciba un número n y
-//devuelva la suma de los cuadrados de todos los números
-//impares que estén entre 1 y n.
+// devuelva el producto (la multiplicación) de los cubos (potencia de 3)
+// de todos los números pares entre 1 y n
 fn main() {
     let n: u16 = 3;
     println!("n es {}", n);
     println!("y su solucion es {}", solucion(n))
 }
 fn solucion(n: u16) -> u16 {
-    let n: u16 = (1..=n).filter(|n| n % 2 != 0).map(|n| n.pow(2)).sum();
+    let n: u16 = (1..=n).filter(|n| n % 2 == 0).map(|n| n.pow(3)).product();
     n
 }

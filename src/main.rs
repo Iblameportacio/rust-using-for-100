df9f30aacd195f5cbcbb47f61780454d369242d6
Iblameportacio <gm.portacio@gmail.com>
2026-06-26 15:25:31 -0500

    tablas de multiplicar del 1 al 10 con formato alineado

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-//Crea una función que reciba un número n y
-// devuelva el producto (la multiplicación) de los cubos (potencia de 3)
-// de todos los números pares entre 1 y n
+//Tabla de multiplicar del 1 al 10 con formato alineado (usa {:>3})
+// im back i guess
 fn main() {
-    let n: u16 = 3;
-    println!("n es {}", n);
-    println!("y su solucion es {}", solucion(n))
-}
-fn solucion(n: u16) -> u16 {
-    let n: u16 = (1..=n).filter(|n| n % 2 == 0).map(|n| n.pow(3)).product();
-    n
+    for i in 1..=10 {
+        println!("esta es la tabla del: {}", i);
+        for z in 1..=10 {
+            let solved = z * i;
+            println!("{:>3} * {:>3} = {:>3}", i, z, solved);
+        }
+        println!(""); //print vacío para un salto entre las líneas
+    }
 }

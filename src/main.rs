406da7bbca8da2f740d1bfb1fe5ddfc1d576a87b
Iblameportacio <gm.portacio@gmail.com>
2026-06-27 02:02:17 -0500

    la secuencia de tribonacci

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-//Suma de la serie 1 + 1/2 + 1/3 + ... + 1/n
+//Números de Tribonacci (como Fibonacci pero suma los 3 anteriores)
 fn main() {
-    let n = 25;
-    let mut suma: f64 = 1.0;
-    for i in 2..=n {
-        suma = suma + 1.0 / (i as f64);
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 1;
+    for _i in 0..10 {
+        let i = a + b + c;
+        let solucion = a;
+        println!("{:?}", solucion);
+        a = b;
+        b = c;
+        c = i;
     }
-    println!("{}", suma)
 }

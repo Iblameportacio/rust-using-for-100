597748521a80260ae48c7c4ea13b92495181c2f7
Iblameportacio <gm.portacio@gmail.com>
2026-06-27 01:54:50 -0500

    suma de la serie uno mas uno sobre n de 2 hasta n

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
-//Collatz: cuenta los pasos para llegar a 1 desde n
-// ESTO SI QUE ES EMOCIONANTE, LA CONJETURA DE COLLATZ JAJAJAJJAJAJA
-// La regla dice que si eliges cualquier número entero positivo y
-// sigues un juego de dos reglas simples, tarde o temprano siempre vas a terminar llegando al número 1
-// la ecuación es 3n+1 si el numero es impar lo multiplicas por tres y le sumas uno
-// si es par lo divides por dos y así
+//Suma de la serie 1 + 1/2 + 1/3 + ... + 1/n
 fn main() {
-    let mut numerito: i32 = 43;
-    println!("para que el numero {} sea uno", numerito);
-    let mut pasos = 0;
-    loop {
-        if numerito % 2 == 0 {
-            numerito = numerito / 2;
-        } else {
-            numerito = numerito * 3 + 1;
-        }
-        pasos += 1;
-        if numerito == 1 {
-            break;
-        }
+    let n = 25;
+    let mut suma: f64 = 1.0;
+    for i in 2..=n {
+        suma = suma + 1.0 / (i as f64);
     }
-    println!("fueron necesarios dar {} pasos", pasos)
+    println!("{}", suma)
 }

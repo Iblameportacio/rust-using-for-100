2eaae8bcf2a50eb8c941931240f6c8e8f59a9a82
Iblameportacio <gm.portacio@gmail.com>
2026-06-26 22:49:43 -0500

    suma de los dígitos de un número

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-//Cuenta regresiva de n hasta 0 con loop (no for)
+//Suma de dígitos de un número (ej: 1234 → 10)
 fn main() {
-    let mut n = 20;
-    loop {
-        println!("{}", n);
-        n -= 1;
-        if n < 0 {
-            break;
-        }
+    let mut numero = 1234;
+    let mut suma = 0;
+
+    // El bucle se repite mientras el número conserve dígitos
+    while numero > 0 {
+        let ultimo_digito = numero % 10;
+
+        // 2. Súmalo a tu variable acumuladora 'suma'
+        suma += ultimo_digito;
+
+        // 3. Quítale el último dígito al número dividiéndolo entre 10
+        numero /= 10;
     }
+
+    println!("La suma de los dígitos es: {}", suma); // Debería dar 10
 }

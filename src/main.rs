40a7186f3ef5abc3a1513e958acbd7802fa4d1d0
Iblameportacio <gm.portacio@gmail.com>
2026-06-26 21:20:22 -0500

    cuenta regresiva de un numero n con un loop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-//pirámide centrada de asteriscos de n filas
+//Cuenta regresiva de n hasta 0 con loop (no for)
 fn main() {
-    let filas_totales = 20;
-
-    for i in 1..=filas_totales {
-        // 1. Calculamos cuántos espacios y asteriscos tocan en esta fila
-        let espacios_necesarios = filas_totales - i;
-        let asteriscos_necesarios = (2 * i) - 1;
-
-        // 2. Creamos los hilos de texto repitiendo los caracteres
-        let bloques_espacio = " ".repeat(espacios_necesarios);
-        let bloques_asterisco = "*".repeat(asteriscos_necesarios);
-
-        // 3. Juntamos ambos e imprimimos
-        println!("{}{}", bloques_espacio, bloques_asterisco);
+    let mut n = 20;
+    loop {
+        println!("{}", n);
+        n -= 1;
+        if n < 0 {
+            break;
+        }
     }
 }

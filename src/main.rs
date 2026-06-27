9d0fb266904839b142d88cef73d5cee4d798b23d
Iblameportacio <gm.portacio@gmail.com>
2026-06-27 01:14:18 -0500

    verificar si un numero es abundante

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
-//Suma de dígitos de un número (ej: 1234 → 10)
+//Verifica si un número es abundante (suma divisores > n)
+// Para saber si un número es abundante,
+// primero tenemos que entender qué es un divisor propio.
+// Un divisor propio de un número $n$ es cualquier número entero
+// positivo más pequeño que él que lo divide exactamente
+// (es decir, que el residuo de la división sea cero).
+// si sumamos esos numero y el resultado es mayor a n
+// entonces es un número abundante
 fn main() {
-    let mut numero = 1234;
+    let numero = 12; //queremos saber si este número es abundante
+    let mut es_abundante = false;
+
     let mut suma = 0;
-
-    // El bucle se repite mientras el número conserve dígitos
-    while numero > 0 {
-        let ultimo_digito = numero % 10;
-
-        // 2. Súmalo a tu variable acumuladora 'suma'
-        suma += ultimo_digito;
-
-        // 3. Quítale el último dígito al número dividiéndolo entre 10
-        numero /= 10;
+    for i in 1..numero {
+        if numero % i == 0 {
+            suma += i;
+        }
     }
-
-    println!("La suma de los dígitos es: {}", suma); // Debería dar 10
+    if suma > numero {
+        es_abundante = true;
+    }
+    println!("el numero {} es abundante? {}", numero, es_abundante)
 }
